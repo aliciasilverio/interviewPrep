fb1df792a69a16165d36584950f9d22bdd44d6f9
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-10-05 14:41:11 -0400

    first hash

---
+++ Hash/hash.js
@@ -0,0 +1,12 @@
+let user = {
+    age: 54,
+    name: 'Kylie',
+    magic: true,
+    scream: function(){
+        console.log('AHHHHHH!');
+    }
+}
+
+user.age // 0(1)
+user.spell = 'abra kadabra'
+user.scream();
