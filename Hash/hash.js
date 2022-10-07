36db841ce4910d179256f1d78196549822057060
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-10-07 15:23:34 -0400

    linked list

--- Hash/hash.js
+++ Hash/hash.js
@@ -36,3 +36,32 @@ keys() {
     return result; 
   }
 
+  // Google Question
+// Given an array = [2, 5, 1, 2, 3, 5, 1, 2, 4]:
+// It should return 2
+
+// Given an array = [2, 1, 1, 2, 3, 5, 1, 2, 4]:
+// It should return 1
+
+// Given an array = [2, 3, 4, 5]:
+// It should return undefined
+
+function firstRecurringCharacter(input){
+    for (let i = 0; i < input.length; i++){
+
+    }
+}
+
+firstRecurringCharacter([2, 5, 1, 2, 3, 5, 1, 2, 4])
+
+
+// Linked List
+
+const basket = ['apples', 'grapes', 'pears'];
+
+linked list: apples --> grapes --> pears
+
+apples
+8947 --> grapes
+    8742 --> pears
+        372 --> null
